b40b7284bcfe3ee75cf66f95ae89b5bdb45ca60d
Pavan <pavanghuge2@gmail.com>
2023-11-24 19:12:08 +0530

    signup created

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,19 +1,31 @@
-import React, { useEffect } from 'react'
+import React, { useState } from 'react'
 import logo from "./logo.jpg"
 import { Link } from "react-router-dom"
 import "./signup.css"
 
 
 export default function Signup() {
+    const [name, setName] = useState("")
+    const [email, setEmail] = useState("")
+    const [userName, setUserName] = useState("")
+    const [password, setPassword] = useState("")
 
-    const fetchdata=async()=>{
-        const response =await fetch("http://localhost:5000")
-        const data=await response.json()
-        console.log(data)
+    const postdata = () => {
+       fetch("http://localhost:5000/signup",{
+        method:"post",
+        headers:{
+            "Content-Type":"application/json"
+        },
+        body:JSON.stringify({
+       name:name,
+       userName:userName,
+       email:email,
+       password:password
+
+        })
+       }).then(res=>res.json())
+       .then(data=>{console.log(data)})
     }
- useEffect(()=>{
-fetchdata()
- },[])
 
     return (
         <div className='signup'>
@@ -26,16 +38,16 @@ fetchdata()
                     </p>
 
                     <div>
-                        <input type='email' placeholder='Enter Your Email' id="email" name="email" />
+                        <input type='email' placeholder='Enter Your Email' id="email" name="email" value={name} onChange={(e) => { setName(e.target.value) }} />
                     </div>
                     <div>
-                        <input type="text" placeholder='Enter Your Name ' name='name' id="name" />
+                        <input type="text" placeholder='Enter Your Name ' name='name' id="name" value={email} onChange={(e) => { setEmail(e.target.value) }} />
                     </div>
                     <div>
-                        <input type='text' placeholder='Enter Your Username' id="username" name="username" />
+                        <input type='text' placeholder='Enter Your Username' id="username" name="username" value={userName} onChange={(e) => { setUserName(e.target.value) }} />
                     </div>
                     <div>
-                        <input type='password' placeholder='password' id="password" name='password' />
+                        <input type='password' placeholder='password' id="password" name='password' value={password} onChange={(e) => { setPassword(e.target.value) }} />
                     </div>
                     <p
                         className="loginpara"
@@ -44,15 +56,15 @@ fetchdata()
                         By signing up , you agree to to out Terms, <br />
                         privacy policy and cookies policy
                     </p>
-                    
-                    <button type='summit'style={{"width":"80%", "padding":"4px", "background-color":"rgb(75, 75, 234);","cursor":"pointer"}}>SignUp</button>
-                    </div>
 
-                    <div className='form2'>
-                        Already have account ?
-                        <span> <Link to="/signin">SignIn</Link></span>
-                    </div>
-                
+                    <button type='summit' style={{ "width": "80%", "padding": "4px", "background-color": "rgb(75, 75, 234);", "cursor": "pointer" }} onClick={() => { postdata() }}>SignUp</button>
+                </div>
+
+                <div className='form2'>
+                    Already have account ?
+                    <span> <Link to="/signin">SignIn</Link></span>
+                </div>
+
 
             </div>
 
